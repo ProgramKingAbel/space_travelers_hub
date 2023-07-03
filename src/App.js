4a975eef50e26caac6b2dda951503f5fdffc83bc
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-03 17:46:04 +0100

    Add routes to App Pages/ Frontend Displays

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-/** @format */
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+import { DragonsPage, MissionsPage, MyProfilePage, RocketsPage } from './Pages';
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path='/' element={<Layout}>
+      <Route index element={<RocketsPage />} />
+      <Route path='Dragons' element={<DragonsPage />} />
+      <Route path='Missions' element={<MissionsPage />} />
+      <Route path='Profile' element={<MyProfilePage />} />
+    </Route>
+  </Routes>
+)
 
-export default App;
+export default App
+
