77cfdc60e9967f6b207595a32cf8caef44803086
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-03 17:33:30 +0100

    Implement Nav and Outlet in shared component

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+      <div>
+          <Navbar />
+          <Outlet />
+    </div>
   )
 }
 
