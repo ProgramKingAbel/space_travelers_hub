35d9285a3533a27ba922b69db6bf265f8ff43587
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-03 17:36:55 +0100

    Implement <BrowserRouter/> to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-/** @format */
-
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
