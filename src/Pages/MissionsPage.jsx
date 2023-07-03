562750c3108cdaee747fab7d99b2a3ed613b6186
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-03 17:17:37 +0100

    Add App Pages

---
+++ src/Pages/MissionsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MissionsPage = () => {
+  return (
+    <div>MissionsPage</div>
+  )
+}
+
+export default MissionsPage
---
+++ src/Pages/RocketsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RocketsPage = () => {
+  return (
+    <div>RocketsPage</div>
+  )
+}
+
+export default RocketsPage
