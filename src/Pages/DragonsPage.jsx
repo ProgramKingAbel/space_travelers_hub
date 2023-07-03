8bfee131aa7281f0c11ddd84155bf32e75deeeb3
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-03 18:36:27 +0100

    install axios and react-redux dependancies

---
+++ src/Pages/DragonsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DragonsPage = () => {
+  return (
+    <div>DragonsPage</div>
+  )
+}
+
+export default DragonsPage
---
+++ src/Pages/MyProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyProfilePage = () => {
+  return (
+    <div>MyProfilePage</div>
+  )
+}
+
+export default MyProfilePage
