e46b118a4f0005f8d868e85941e01192fa53270e
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-04 19:46:50 +0100

    Add bootstrap css for styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
