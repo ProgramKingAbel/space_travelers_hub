b110721506aa0722bba6c826dac196d71494da62
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-04 14:16:41 +0100

    dispatch the fetchrockets action

--- src/Pages/RocketsPage.jsx
+++ src/Pages/RocketsPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/features/rockets/rocketsSlice';
 
-const RocketsPage = () => (
-  <div>RocketsPage</div>
-);
+const RocketsPage = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocket.rockets);
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+  console.log(rockets);
+  return (
+    <div>RocketsPage</div>
+  );
+};
 
 export default RocketsPage;
