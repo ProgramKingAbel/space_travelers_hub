b22c8877b2f950a393dac4edaa78d2b549b7e94a
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-05 01:22:55 +0100

    Implement Elements on default rockets view

--- src/Pages/RocketsPage.jsx
+++ src/Pages/RocketsPage.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import { fetchRockets } from '../redux/features/rockets/rocketsSlice';
 
 const RocketsPage = () => {
@@ -10,13 +16,24 @@ const RocketsPage = () => {
   }, []);
 
   return (
-    <ul>
+    <Container>
       {
         rockets.map((rocket) => (
-          <li key={rocket.id}>{ rocket.rocket_name }</li>
+          <Row key={rocket.id} style={{ marginTop: '15px', marginBottom: '15px' }}>
+            <Col sm={3}>
+              <Image src={rocket.flickr_images[0]} thumbnail />
+            </Col>
+            <Col sm={9}>
+              <Stack gap={1}>
+                <h2>{rocket.rocket_name}</h2>
+                <p>{rocket.description}</p>
+              </Stack>
+              <Button variant="primary">Reserve Rocket</Button>
+            </Col>
+          </Row>
         ))
       }
-    </ul>
+    </Container>
   );
 };
 
