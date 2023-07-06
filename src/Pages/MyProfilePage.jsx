5fb33a348e3f65150f7a472c7599d61977d258cc
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-06 13:03:52 +0100

    Remove debugging console statements

--- src/Pages/MyProfilePage.jsx
+++ src/Pages/MyProfilePage.jsx
@@ -8,15 +8,12 @@ import Col from 'react-bootstrap/Col';
 const MyProfilePage = () => {
   const { rockets } = useSelector((state) => state.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
-  console.log('reservedRockets', reservedRockets);
 
   const { spaceMission } = useSelector((state) => state.spaceMission);
   const missions = spaceMission.filter((mission) => mission.statusCheck === true);
-  console.log('missions', missions);
 
   const { dragons } = useSelector((state) => state.dragons);
   const reservedDragons = dragons.filter((dragon) => dragon.reserved === true);
-  console.log('reservedDragons', reservedDragons);
 
   return (
     <Container>
