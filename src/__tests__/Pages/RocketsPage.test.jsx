5213a429a794787235330a34e7347019d0e5f089
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-06 15:07:53 +0100

    Mock actions

--- src/__tests__/Pages/RocketsPage.test.jsx
+++ src/__tests__/Pages/RocketsPage.test.jsx
@@ -4,9 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import RocketsPage from "../../Pages/RocketsPage";
 import { fetchRockets, booked, cancelReservation } from "../../redux/features/rockets/rocketsSlice";
 
-jest.mock('react-redux hooks', () => (
+jest.mock('react-redux', () => (
     {
         useDispatch: jest.fn(),
         useSelector: jest.fn,
     }
 ));
+
+jest.mock('../../redux/features/rockets/rocketsSlice.js', () => ({
+    fetchRockets: jest.fn(),
+    booked: jest.fn(),
+    cancelReservation: jest.fn(),
+}));
+
+describe('RocketsPage', () => {
+    
+})
